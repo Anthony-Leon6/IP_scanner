51fdd7ca17fd260b675671a0f63fd08a1584bf98
Anthony-Leon6 <gryszzz6@gmail.com>
2024-12-17 13:43:22 -0500

    ip_scanner.bat

--- ip_scanner.bat
+++ ip_scanner.bat
@@ -1,36 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBxGVReYAE+/Fb4I5/jHwe+Q4ksSWOY6arPJz7qxKfAs+Uble5goxDRfgM5s
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+
 @echo off
 SET SaveFile="ip_scan_results.txt"
 del %SaveFile% 2>nul
@@ -68,14 +36,5 @@ FOR /L %%i IN (%Start%,1,%End%) DO (
     tracert %IPRange%.%%i >> %SaveFile%
     echo. >> %SaveFile%
     echo Results saved to %SaveFile%.
-
-:: Ask if the user wants to restart
-set /p Restart=Do you want to restart the IP Scanner? (Y/N): 
-if /i "%Restart%"=="Y" goto Start
-if /i "%Restart%"=="N" exit
-
-:: If the input is not Y or N, prompt again
-echo Invalid input. Please enter Y or N.
-goto Start
 )
 echo Scan complete. Results saved to %SaveFile%.
